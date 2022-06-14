b9a237c5c1af876ce58491de64cbaa6b5748bb1d
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-06-14 23:38:56 +0700

    Xay dựng UI phần Sidebar

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,34 @@
+import Menu, { MenuItem } from "./Menu";
+import {
+  HomeIcon,
+  UserGroupIcon,
+  LiveIcon,
+  HomeActiveIcon,
+  UserGroupActiveIcon,
+  LiveActiveIcon,
+} from "~/components/Icons";
+
+import styles from "./Sidebar.module.scss";
+import classNames from "classnames/bind";
+import config from "~/config";
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+  return (
+    <aside className={cx("wrapper")}>
+      <Menu>
+        <MenuItem icon={<HomeIcon />} title="For You" to={config.routes.home} activeIcon={<HomeActiveIcon />} />
+        <MenuItem
+          icon={<UserGroupIcon />}
+          title="Following"
+          to={config.routes.following}
+          activeIcon={<UserGroupActiveIcon />}
+        />
+        <MenuItem icon={<LiveIcon />} title="LIVE" to={config.routes.live} activeIcon={<LiveActiveIcon />} />
+      </Menu>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import Home from "~/pages/Home";
 import Profile from "~/pages/Profile";
 import Upload from "~/pages/Upload";
 import Search from "~/pages/Search";
+import Live from "~/pages/Live";
 import { HeaderOnly } from "~/layouts";
 import config from "~/config";
 
@@ -11,6 +12,7 @@ export const publicRoutes = [
   { path: config.routes.home, component: Home },
   { path: config.routes.following, component: Following },
   { path: config.routes.profile, component: Profile },
+  { path: config.routes.live, component: Live },
   { path: config.routes.search, component: Search, layout: null },
   { path: config.routes.upload, component: Upload, layout: HeaderOnly },
 ];
